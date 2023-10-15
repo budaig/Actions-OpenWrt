d4c35ef3824211571eebecae24fd4885e578f3dd
B1uEx <brux@qq.com>
2023-10-15 09:48:23 +0800

    mod buildenv

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -26,15 +26,15 @@ rm -rf feeds/luci/applications/luci-app-alist
 git clone https://github.com/sbwml/luci-app-alist feeds/packages/net/alist
 # cp -fR feeds/packages/net/alist/luci-app-alist feeds/luci/applications/luci-app-alist
 # Add iperf3server
-cd package
-mkdir kiddin9
-cd kiddin9
-git init
-git remote add -f master https://github.com/kiddin9/openwrt-packages.git
-git config core.sparsecheckout true
-echo "luci-app-iperf3-server" >> .git/info/sparse-checkout
-git pull origin master
-cd openwrt
+# cd package
+# mkdir kiddin9
+# cd kiddin9
+# git init
+# git remote add -f origin https://github.com/kiddin9/openwrt-packages.git
+# git config core.sparsecheckout true
+# echo "luci-app-iperf3-server" >> .git/info/sparse-checkout
+# git pull origin master
+# cd openwrt
 
 ##-----------------Add OpenClash dev core------------------
 # curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
