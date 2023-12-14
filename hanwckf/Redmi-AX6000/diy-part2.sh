2f18b0737c5aa6f2aaef053b095ec0adc070b198
B1uEx <brux@qq.com>
2023-12-14 19:58:46 +0800

    update openclash

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -74,12 +74,12 @@ rm -rf feeds/luci/applications/luci-app-openclash
 mkdir package/luci-app-openclash
 git init package/openclash
 cd package/openclash
-git config --global init.defaultBranch master
+git config --global init.defaultBranch main
 git remote add -f origin https://github.com/kiddin9/openwrt-packages.git
 git config core.sparsecheckout true
 echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
-git branch --set-upstream-to=origin/master master
+git pull --depth 1 origin main
+git branch --set-upstream-to=origin/main main
 cd ../..
 # # 编译 po2lmo (如果有po2lmo可跳过)
 # # pushd luci-app-openclash/tools/po2lmo
