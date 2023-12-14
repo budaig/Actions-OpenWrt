4ad138bd1c6ed36871f33cddc322b82283b2d544
B1uEx <brux@qq.com>
2023-12-14 10:21:08 +0800

    add zerotier

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -23,20 +23,29 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 # update 20.x to 21.x
+
 # # replace ddns-go
 # rm -rf feeds/packages/net/ddns-go
 # rm -rf feeds/luci/applications/luci-app-ddns-go
 # git clone https://github.com/sirpdboy/luci-app-ddns-go feeds/packages/net/ddns-go   #or package/ddns-go
 # cp -fR feeds/packages/net/ddns-go/luci-app-ddns-go feeds/luci/applications/luci-app-ddns-go
+
 # use lucky over ddns-go
 # rm -rf feeds/packages/net/lucky
 # rm -rf feeds/luci/applications/luci-app-lucky
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+
 # replace alist
 rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
 git clone https://github.com/sbwml/luci-app-alist.git package/alist
 # cp -fR feeds/packages/net/alist/luci-app-alist feeds/luci/applications/luci-app-alist
+
+# add chatgpt-web
+# rm -rf feeds/packages/net/luci-app-chatgpt-web
+# rm -rf feeds/luci/applications/luci-app-chatgpt-web
+# git clone https://github.com/sirpdboy/luci-app-chatgpt-web package/chatgpt-web
+
 # add netspeedtest
 # rm -rf feeds/packages/net/netspeedtest
 # rm -rf feeds/luci/applications/luci-app-netspeedtest
@@ -59,13 +68,29 @@ git clone https://github.com/sbwml/luci-app-alist.git package/alist
 # rm -rf ./feeds/luci/themes/luci-theme-argon
 # git clone -b master https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
 
+# replace openclash
+rm -rf feeds/packages/net/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+# 编译 po2lmo (如果有po2lmo可跳过)
+# pushd luci-app-openclash/tools/po2lmo
+# # make && sudo make install
+# popd
+
 ##-----------------Add OpenClash dev core------------------
-# curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-# tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-# chmod +x /tmp/clash >/dev/null 2>&1
-# mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-# mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-# rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##---------------------------------------------------------
 
 # Enable Cache
