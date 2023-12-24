31443332482b43021887370affe8e5b53047d543
B1uEx <brux@qq.com>
2023-12-24 16:57:36 +0800

    update v2raya

--- hanwckf/Redmi-AX6000/diy-TY-part2.sh
+++ hanwckf/Redmi-AX6000/diy-TY-part2.sh
@@ -14,6 +14,17 @@
 # package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
+cat > package/base-files/files/etc/banner << EOF
+  _______                     ________        __
+ |       |.-----.-----.-----.|  |  |  |.----.|  |_
+ |   -   ||  _  |  -__|     ||  |  |  ||   _||   _|
+ |_______||   __|_____|__|__||________||__|  |____|
+          |__| W I R E L E S S   B U D A I
+ -----------------------------------------------------
+ %D %V, %C
+ -----------------------------------------------------
+EOF
+
 # update golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
@@ -28,12 +39,6 @@ git clone https://github.com/sbwml/luci-app-alist.git package/custom/alist
 # rm -rf feeds/luci/applications/luci-app-v2raya
 # git clone https://github.com/v2rayA/v2raya-openwrt package/custom/v2raya
 # # sed -i 's/PKG_VERSION:=2.2.4.1/PKG_VERSION:=2.2.4.6/g' package/custom/v2raya/Makefile
-
-pushd feeds/luci/applications
-rm -rf luci-app-openclash
-git clone --depth 1 -b master https://github.com/vernesong/OpenClash openclash && mv -n openclash/luci-app-openclash luci-app-openclash; rm -rf openclash
-popd
-cd ..
 
 #replace a theme
 # rm -rf ./feeds/luci/themes/luci-theme-argon
@@ -56,13 +61,9 @@ rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 
 ##---------------------------------------------------------
 
-cat > package/base-files/files/etc/banner << EOF
-  _______                     ________        __
- |       |.-----.-----.-----.|  |  |  |.----.|  |_
- |   -   ||  _  |  -__|     ||  |  |  ||   _||   _|
- |_______||   __|_____|__|__||________||__|  |____|
-          |__| W I R E L E S S   B U D A I
- -----------------------------------------------------
- %D %V, %C
- -----------------------------------------------------
-EOF
+
+pushd feeds/luci/applications
+rm -rf luci-app-openclash
+git clone --depth 1 -b master https://github.com/vernesong/OpenClash openclash && mv -n openclash/luci-app-openclash luci-app-openclash; rm -rf openclash
+popd
+cd ..
