33cd0c88070b57c2bde2b7bad0ac83b045cde201
B1uEx <brux@qq.com>
2023-12-24 16:04:13 +0800

    update v2raya

--- hanwckf/Redmi-AX6000/diy-TY-part2.sh
+++ hanwckf/Redmi-AX6000/diy-TY-part2.sh
@@ -24,10 +24,10 @@ rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
 git clone https://github.com/sbwml/luci-app-alist.git package/custom/alist
 
-rm -rf feeds/packages/net/v2raya
-rm -rf feeds/luci/applications/luci-app-v2raya
-git clone https://github.com/v2rayA/v2raya-openwrt package/custom/v2raya
-# sed -i 's/PKG_VERSION:=2.2.4.1/PKG_VERSION:=2.2.4.6/g' package/custom/v2raya/Makefile
+# rm -rf feeds/packages/net/v2raya
+# rm -rf feeds/luci/applications/luci-app-v2raya
+# git clone https://github.com/v2rayA/v2raya-openwrt package/custom/v2raya
+# # sed -i 's/PKG_VERSION:=2.2.4.1/PKG_VERSION:=2.2.4.6/g' package/custom/v2raya/Makefile
 
 rm -rf feeds/luci/applications/luci-app-openclash
 pushd feeds/luci/applications
