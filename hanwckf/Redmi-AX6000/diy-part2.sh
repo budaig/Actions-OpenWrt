9a10cb97013dbe3103d6da79a9f48b8950b87641
B1uEx <brux@qq.com>
2023-12-30 10:22:16 +0800

    readd oc

--- hanwckf/Redmi-AX6000/diy-part2.sh
+++ hanwckf/Redmi-AX6000/diy-part2.sh
@@ -84,12 +84,12 @@ rm -rf feeds/luci/applications/luci-app-v2raya
 git clone https://github.com/v2rayA/v2raya-openwrt package/custom/v2raya
 
 # ##-------------- GeoIP 数据库 -----------------------------
-curl -sL -m 30 --retry 2 https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -o /tmp/geoip.dat
-mv /tmp/geoip.dat package/custom/v2raya/luci-app-v2raya/root/usr/share/xray/geoip.dat >/dev/null 2>&1
+# curl -sL -m 30 --retry 2 https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -o /tmp/geoip.dat
+# mv /tmp/geoip.dat package/custom/v2raya/luci-app-v2raya/root/usr/share/xray/geoip.dat >/dev/null 2>&1
 
-# ##-------------- GeoSite 数据库 ---------------------------
-curl -sL -m 30 --retry 2 https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -o /tmp/geosite.dat
-mv /tmp/geosite.dat package/custom/v2raya/luci-app-v2raya/root/usr/share/xray/geosite.dat >/dev/null 2>&1
+# # ##-------------- GeoSite 数据库 ---------------------------
+# curl -sL -m 30 --retry 2 https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -o /tmp/geosite.dat
+# mv /tmp/geosite.dat package/custom/v2raya/luci-app-v2raya/root/usr/share/xray/geosite.dat >/dev/null 2>&1
 # ##---------------------------------------------------------
 
 # replace a theme
