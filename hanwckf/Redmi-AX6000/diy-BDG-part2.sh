a46415c1f113f2ff3beaa2c6412d4d90b4b11324
B1uEx <brux@qq.com>
2024-04-30 13:08:30 +0800

    update sh2

--- hanwckf/Redmi-AX6000/diy-BDG-part2.sh
+++ hanwckf/Redmi-AX6000/diy-BDG-part2.sh
@@ -174,13 +174,14 @@ sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:='"$SMAERTDNS_SHA"'/g' packa
 sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$SMARTDNS_VER"'/g' package/custom/luci-app-smartdns/Makefile
 sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' package/custom/luci-app-smartdns/Makefile
 ## add anti-ad data
+# sed -i '/PKG_MIRROR_HASH:=/d' package/custom/smartdns/Makefile 上面Makefile中已将此行删除所以修改后的makefile中下列要增加的行由原48改47-原63变62
 sed -i '47a\/etc/smartdns/reject.conf' package/custom/smartdns/Makefile
-sed -i '63i\	$(INSTALL_CONF) $(PKG_BUILD_DIR)/package/openwrt/reject.conf $(1)/etc/smartdns/reject.conf' package/custom/smartdns/Makefile
-cat package/custom/smartdns/Makefile
-sleep 8
+sed -i '62i\	$(INSTALL_CONF) $(PKG_BUILD_DIR)/package/openwrt/reject.conf $(1)/etc/smartdns/reject.conf' package/custom/smartdns/Makefile
+# cat package/custom/smartdns/Makefile
+# sleep 8
 mkdir -p package/custom/smartdns/root/etc/smartdns
 ls -dR package/custom/smartdns/root/etc/smartdns
-sleep 8
+sleep 1
 curl -sL -m 30 --retry 2 https://anti-ad.net/anti-ad-for-smartdns.conf -o /tmp/reject.conf
 mv /tmp/reject.conf package/custom/smartdns/root/etc/smartdns/reject.conf >/dev/null 2>&1
 # ## ---------------------------------------------------------
