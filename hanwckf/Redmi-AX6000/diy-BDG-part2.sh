0aca8216900c1ddf43d9aea98ddeb029ed5aa167
B1uEx <brux@qq.com>
2024-05-30 11:24:34 +0800

    update sh2

--- hanwckf/Redmi-AX6000/diy-BDG-part2.sh
+++ hanwckf/Redmi-AX6000/diy-BDG-part2.sh
@@ -88,12 +88,12 @@ git clone https://github.com/sbwml/luci-app-alist.git package/custom/alist
 rm -rf feeds/luci/applications/luci-app-lucky
 
 # #/etc/config/lucky.daji/lucky.conf
-# git clone https://github.com/gdy666/luci-app-lucky.git package/custom/lucky
-# sleep 1
+git clone https://github.com/gdy666/luci-app-lucky.git package/custom/lucky
+sleep 1
 # ## customize lucky ver
 # # wget https://www.daji.it:6/files/$(PKG_VERSION)/$(PKG_NAME)_$(PKG_VERSION)_Linux_$(LUCKY_ARCH).tar.gz
-# lkver=2.6.2
-# sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$lkver"'/g;s/github.com\/gdy666\/lucky\/releases\/download\/v/www.daji.it\:6\/files\//g' package/custom/lucky/lucky/Makefile
+lkver=2.6.2
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$lkver"'/g;s/github.com\/gdy666\/lucky\/releases\/download\/v/www.daji.it\:6\/files\//g' package/custom/lucky/lucky/Makefile
 
 # wget https://github.com/gdy666/lucky-files$(PKG_VERSION)/$(PKG_NAME)_$(PKG_VERSION)_Linux_$(LUCKY_ARCH).tar.gz
 # lkver=2.5.1
@@ -101,13 +101,13 @@ rm -rf feeds/luci/applications/luci-app-lucky
 
 
    #/etc/lucky/lucky.conf
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/custom/lucky
-sleep 1
+# git clone https://github.com/sirpdboy/luci-app-lucky.git package/custom/lucky
+# sleep 1
 # ## customize lucky ver
 # # wget https://www.daji.it:6/files/$(PKG_VERSION)/$(PKG_NAME)_$(PKG_VERSION)_Linux_$(LUCKY_ARCH).tar.gz
-lkver=2.9.0
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$lkver"'/g;s/github.com\/gdy666\/lucky\/releases\/download\/v/www.daji.it\:6\/files\//g' package/custom/lucky/lucky/Makefile
-sed -i '/PKG_SOURCE_VERSION:=/d' package/custom/lucky/lucky/Makefile
+# lkver=2.6.2
+# sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$lkver"'/g;s/github.com\/gdy666\/lucky\/releases\/download\/v/www.daji.it\:6\/files\//g' package/custom/lucky/lucky/Makefile
+# sed -i '/PKG_SOURCE_VERSION:=/d' package/custom/lucky/lucky/Makefile
 
 # cat package/custom/lucky/lucky/Makefile
 # ## ---------------------------------------------------------
