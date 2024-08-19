59302cdc7b978fd873677cf42755d59fbac8783b
B1uEx <brux@qq.com>
2024-08-19 18:51:44 +0800

    update sh2

--- immortalwrt/Redmi-AX6000/diy-BDG-part2.sh
+++ immortalwrt/Redmi-AX6000/diy-BDG-part2.sh
@@ -52,7 +52,7 @@ done
 
 # ## update golang 20.x to 21.x
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
 # ## -------------- adguardhome ---------------------------
 rm -rf feeds/packages/net/adguardhome
@@ -83,13 +83,13 @@ mv package/diy/alist/luci-app-alist feeds/luci/applications/luci-app-alist
 
 # ## customize alist ver
 # # sleep 1
-# alver=3.33.0
-# alwebver=3.33.0
-# alsha256=($(curl -sL https://codeload.github.com/alist-org/alist/tar.gz/v$alver | shasum -a 256))
-# alwebsha256=($(curl -sL https://github.com/alist-org/alist-web/releases/download/$alwebver/dist.tar.gz | shasum -a 256))
-# echo alist $alver sha256=$alsha256
-# echo alist-web $alver sha256=$alwebsha256
-# sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$alver"'/g;s/PKG_WEB_VERSION:=.*/PKG_WEB_VERSION:='"$alwebver"'/g;s/PKG_HASH:=.*/PKG_HASH:='"$alsha256"'/g;26 s/  HASH:=.*/  HASH:='"$alwebsha256"'/g' feeds/packages/net/alist/Makefile
+alver=3.33.0
+alwebver=3.33.0
+alsha256=($(curl -sL https://codeload.github.com/alist-org/alist/tar.gz/v$alver | shasum -a 256))
+alwebsha256=($(curl -sL https://github.com/alist-org/alist-web/releases/download/$alwebver/dist.tar.gz | shasum -a 256))
+echo alist $alver sha256=$alsha256
+echo alist-web $alver sha256=$alwebsha256
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$alver"'/g;s/PKG_WEB_VERSION:=.*/PKG_WEB_VERSION:='"$alwebver"'/g;s/PKG_HASH:=.*/PKG_HASH:='"$alsha256"'/g;26 s/  HASH:=.*/  HASH:='"$alwebsha256"'/g' feeds/packages/net/alist/Makefile
 
 # change default port: version 3.33.0 and up
 # sed -i 's/5244/5246/g' package/diy/alist/alist/files/alist.config
