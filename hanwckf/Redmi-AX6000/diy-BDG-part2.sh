bb9d5494f001172df244abab903bc6aee53f1929
B1uEx <brux@qq.com>
2024-08-30 19:09:21 +0800

    update sh2

--- hanwckf/Redmi-AX6000/diy-BDG-part2.sh
+++ hanwckf/Redmi-AX6000/diy-BDG-part2.sh
@@ -53,7 +53,7 @@ done
 # ## update golang 20.x to 21.x
 # nl feeds/packages/lang/golang/golang/Makefile   #21.02 org ver1.19
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 # use
 # cp ${GITHUB_WORKSPACE}/_modFiles/golang-values.mk feeds/packages/lang/golang/golang-values.mk
 # 21.x to use 21.4
