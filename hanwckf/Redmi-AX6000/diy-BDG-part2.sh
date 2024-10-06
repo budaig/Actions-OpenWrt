cd190989668f20dd1c1446368183c718e7733f5d
B1uEx <brux@qq.com>
2024-10-06 20:32:36 +0800

    update sh2

--- hanwckf/Redmi-AX6000/diy-BDG-part2.sh
+++ hanwckf/Redmi-AX6000/diy-BDG-part2.sh
@@ -324,9 +324,14 @@ rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/luci/applications/luci-app-mosdns
 
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/diy/mosdns
-# sed -i '9 s/.*/LUCI_DEPENDS:=+mosdns +jsonfilter +curl +v2dat/g' package/diy/mosdns/luci-app-mosdns/Makefile
-# sed -i 's/share\/v2ray/share\/xray/g' package/diy/mosdns/luci-app-mosdns/root/usr/share/mosdns/mosdns.sh
+## gitclone sbwml/luci-app-mosdns
+1. gitclone + mod makfile   -  prefer 1.
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/diy/mosdns
+sed -i '9 s/.*/LUCI_DEPENDS:=+mosdns +jsonfilter +curl +v2dat/g' package/diy/mosdns/luci-app-mosdns/Makefile
+sed -i 's/share\/v2ray/share\/xray/g' package/diy/mosdns/luci-app-mosdns/root/usr/share/mosdns/mosdns.sh
+
+# 2. clone mod dir
 
 # customize to use 5.3.x (TODO:531 include both https://github.com/sbwml/v2ray-geodata and https://github.com/yichya/openwrt-xray-geodata-cut Makefile)
 # mkdir -p package/diy/mosdns
@@ -350,18 +355,25 @@ rm -rf feeds/luci/applications/luci-app-mosdns
     # echo "mosdnsgeodataMakefile copy failed"
 # fi
 
+## gitclone QiuSimons/openwrt-mos
+# 1. gitclone + mod makfile
+
 # git clone https://github.com/QiuSimons/openwrt-mos -b master package/diy/mosdns
+# rm -rf package/diy/mosdns/v2ray-geodata
+
+# 2. clone mod dir   -  prefer 2.
 # customize to use https://github.com/yichya/openwrt-xray-geodata-cut Makefile)
-mkdir -p package/diy/mosdns
-# cp -rf ${GITHUB_WORKSPACE}/_modFiles/openwrt-mos-241005/* package/diy/mosdns/
-mv -f ${GITHUB_WORKSPACE}/_modFiles/openwrt-mos-241005/* package/diy/mosdns/
-if [ $? -eq 0 ]; then
-    echo "mosdns dir copied"
-else
-    echo "mosdns dir copy failed"
-fi
-chmod +x package/diy/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
-# ls package/diy/mosdns
+
+# mkdir -p package/diy/mosdns
+
+# mv -f ${GITHUB_WORKSPACE}/_modFiles/openwrt-mos-241005/* package/diy/mosdns/
+# if [ $? -eq 0 ]; then
+    # echo "mosdns dir copied"
+# else
+    # echo "mosdns dir copy failed"
+# fi
+# chmod +x package/diy/mosdns/luci-app-mosdns/root/etc/init.d/mosdns
+# chmod +x package/diy/mosdns/
 
 # ## ---------------------------------------------------------
 
