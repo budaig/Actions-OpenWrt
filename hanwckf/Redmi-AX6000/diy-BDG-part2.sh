38f0b4eea168e71a9a827ec01db5521df86577aa
budaig <imovomi@gmail.com>
2025-06-30 08:42:59 +0800

    update sh2

--- hanwckf/Redmi-AX6000/diy-BDG-part2.sh
+++ hanwckf/Redmi-AX6000/diy-BDG-part2.sh
@@ -78,15 +78,23 @@ git clone https://github.com/sbwml/luci-app-openlist -b main package/diy/openlis
 # [ -f "/www/luci-static/resources/ui.js" ] && echo "Yes" || echo "No"
 # 返回 Yes 表示支持，返回 No 表示不支持。
 
+## ------------------- X. Lite openlist & frontend ver
+cp -f ${GITHUB_WORKSPACE}/_modFiles/2openlist/4lite.Makefile package/diy/openlist/openlist/Makefile
+if [ $? -eq 0 ]; then
+    echo "4lite.Makefile copied"
+else
+    echo "4lite.Makefile copy failed"
+fi
+
 ## ------------------- A. customize openlist & frontend ver
 # sleep 1
-# olver=4.0.2
+# olver=4.0.5
 # olsha256=($(curl -sL https://codeload.github.com/OpenListTeam/OpenList/tar.gz/v$olver | shasum -a 256))
 # echo openlist v$olver sha256=$olsha256
 # sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"$olver"'/g;s/PKG_HASH:=.*/PKG_HASH:='"$olsha256"'/g' package/diy/openlist/openlist/Makefile
 
 # curl -sL https://github.com/OpenListTeam/OpenList-Frontend/releases/download/v4.0.2/openlist-frontend-dist-v4.0.2.tar.gz | shasum -a 256
-# olfrontendver=4.0.2
+# olfrontendver=4.0.5
 # olfrontendsha256=($(curl -sL https://github.com/OpenListTeam/OpenList-Frontend/releases/download/v$olfrontendver/openlist-frontend-dist-v$olfrontendver.tar.gz | shasum -a 256)) 
 # echo openlistfrontend v$olfrontendver sha256=$olfrontendsha256
 # sed -i 's/PKG_WEB_VERSION:=.*/PKG_WEB_VERSION:='"$olfrontendver"'/g;27 s/  HASH:=.*/  HASH:='"$olfrontendsha256"'/g' package/diy/openlist/openlist/Makefile
@@ -112,7 +120,7 @@ git clone https://github.com/sbwml/luci-app-openlist -b main package/diy/openlis
 # sed -i 's/PKG_WEB_VERSION:=.*/PKG_WEB_VERSION:='"$olfrontendver"'/g;s/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:='"$OpenList_date"'/g;s/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:='"$OpenList_SHA"'/g;s/PKG_MIRROR_HASH.*/PKG_MIRROR_HASH:=skip/g;s/  HASH:=.*/  HASH:='"$olfrontendsha256"'/g' package/diy/openlist/openlist/Makefile
 
 ## -------------------. del flash *.swf file preview dir ruffle
-sed -i '83 i \	$(RM) -rf $(PKG_BUILD_DIR)/public/dist/static/ruffle' package/diy/openlist/openlist/Makefile
+# sed -i '83 i \	$(RM) -rf $(PKG_BUILD_DIR)/public/dist/static/ruffle' package/diy/openlist/openlist/Makefile
 
 # or
 # cp -f ${GITHUB_WORKSPACE}/_modFiles/2openlist/4mod.Makefile package/diy/openlist/openlist/Makefile
@@ -483,12 +491,19 @@ else
     echo "sitefcm copy failed"
 fi
 
-cp -f ${GITHUB_WORKSPACE}/_modFiles/2smartdns/blockADcooka.mos package/diy/luci-app-smartdns/root/etc/smartdns/blockADcooka.txt
+cp -f ${GITHUB_WORKSPACE}/_modFiles/2smartdns/hostsblockcustom package/diy/luci-app-smartdns/root/etc/smartdns/hostsblockcustom
 if [ $? -eq 0 ]; then
-    echo "blockADcooka copied"
+    echo "hostsblockcustom copied"
 else
-    echo "blockADcooka copy failed"
+    echo "hostsblockcustom copy failed"
 fi
+
+# cp -f ${GITHUB_WORKSPACE}/_modFiles/2smartdns/blockADcooka.mos package/diy/luci-app-smartdns/root/etc/smartdns/blockADcooka.conf
+# if [ $? -eq 0 ]; then
+    # echo "blockADcooka copied"
+# else
+    # echo "blockADcooka copy failed"
+# fi
 
 cp -f ${GITHUB_WORKSPACE}/_modFiles/2smartdns/resolv.conf.auto package/diy/luci-app-smartdns/root/etc/smartdns/resolv.conf.auto
 if [ $? -eq 0 ]; then
