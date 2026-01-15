e0e95d44ec7108a2b872bd429726502c350669a9
budaig <imovomi@gmail.com>
2026-01-15 14:23:37 +0800

    update sh2

--- hanwckf/Redmi-AX6000/diy-BDG-part2.sh
+++ hanwckf/Redmi-AX6000/diy-BDG-part2.sh
@@ -250,7 +250,7 @@ git clone -b main https://github.com/budaig/luci-app-parentcontrol package/diy/p
 
 # ##  -------------- Passwall ---------------------------
 rm -rf feeds/luci/applications/luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall -b main package/diy/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall -b main package/diy/passwall
 
 # ##  -------------- Passwall2 ---------------------------
 rm -rf feeds/luci/applications/luci-app-passwall2
@@ -289,15 +289,16 @@ git clone https://github.com/yichya/openwrt-xray -b master package/diy/openwrt-x
 ##  -------------- luci app xray ---------------------------
 rm -rf feeds/luci/applications/luci-app-xray || echo "Failed to delete /luci-app-xray"
 
-# git clone -b master https://github.com/rafmilecki/luci-app-xjay package/diy/luci-app-xjay
+git clone -b master https://github.com/rafmilecki/luci-app-xjay package/diy/luci-app-xjay
+sed -i '526 s/true/false/' package/diy/luci-app-xjay/files/luci/outbound.js
 
 git clone -b main https://github.com/quanljh/luci-app-simple-xray package/diy/luci-app-simplexray
 sed -i '3i PKG_NAME:=luci-app-simple-xray\nPKG_VERSION:=0.1\nPKG_RELEASE:=1' package/diy/luci-app-simplexray/luci-app-simple-xray/Makefile
 
 ## for OpenWrt 21.02.0 and later
-git clone -b luci2 https://github.com/bi7prk/luci-app-xray.git package/diy/luci-app-xray   #for 21.02 and up
+# git clone -b luci2 https://github.com/bi7prk/luci-app-xray.git package/diy/luci-app-xray   #for 21.02 and up   安装不上
 
-# git clone https://github.com/honwen/luci-app-xray.git package/diy/luci-app-xray   #for openwrt 21.02 兼容SagerNet/v2ray-core
+git clone https://github.com/honwen/luci-app-xray.git package/diy/luci-app-xray   #for openwrt 21.02 兼容SagerNet/v2ray-core
 
 ## or ttimasdf xray/service name xapp/ luci for 21.02 and up---------------
 # git clone https://github.com/ttimasdf/luci-app-xray -b master package/diy/luci-app-xapp   #for 19.07
